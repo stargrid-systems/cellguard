1347289c2682f20822910b0eaae97c11dfd0c24b
Simon Berger <simon@siku2.io>
2025-12-16 23:36:36 +0000

    temp: working cobs

--- libraries/cellagent-protocol/src/lib.rs
+++ libraries/cellagent-protocol/src/lib.rs
@@ -1,1 +1,44 @@
 #![no_std]
+
+use zerocopy::{
+    FromBytes, Immutable, IntoBytes, KnownLayout, LittleEndian, TryFromBytes, U16, Unaligned,
+};
+
+pub mod cobs; // TODO: pub to silence unused warning
+
+// TODO: add start of frame marker
+
+#[derive(FromBytes, Immutable, IntoBytes, KnownLayout, Unaligned)]
+#[repr(C)]
+pub struct Packet {
+    pub header: PacketHeader,
+    pub payload: [u8],
+}
+
+#[derive(FromBytes, Immutable, IntoBytes, KnownLayout, Unaligned)]
+#[repr(C)]
+pub struct PacketHeader {
+    pub id: u8,
+    pub raw_kind: u8,
+    pub len: U16<LittleEndian>,
+    pub crc: U16<LittleEndian>,
+}
+
+#[derive(Clone, Copy, TryFromBytes, Immutable, IntoBytes, KnownLayout, Unaligned)]
+#[repr(u8)]
+#[non_exhaustive]
+pub enum Kind {
+    ReadDeviceId = 1u8.to_le(),
+    ReadSerialNumber = 2u8.to_le(),
+    ReadTemperature = 3u8.to_le(),
+}
+
+/// ATTiny 3-byte device ID.
+#[derive(FromBytes, Immutable, IntoBytes, KnownLayout, Unaligned)]
+#[repr(transparent)]
+pub struct DeviceId([u8; 3]);
+
+/// 10-byte serial number of the ATTiny MCU.
+#[derive(FromBytes, Immutable, IntoBytes, KnownLayout, Unaligned)]
+#[repr(transparent)]
+pub struct SerialNumber([u8; 10]);
